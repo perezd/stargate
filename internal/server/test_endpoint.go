9e2e42d25347272d4dacfd97b7742746cec1e8d7
Derek Perez <derek@perez.earth>
2026-04-26 15:14:53 -0700

    Revert "fix(server): wire server.timeout as context deadline on classify calls"
    This reverts commit 78286ee5f66b3a87d76e24873cdceda97dad2987.

--- internal/server/test_endpoint.go
+++ internal/server/test_endpoint.go
@@ -1,13 +1,11 @@
 package server
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
 	"io"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/limbic-systems/stargate/internal/classifier"
 )
@@ -60,17 +58,7 @@ func (s *Server) handleTest(w http.ResponseWriter, r *http.Request) {
 	classifyReq.DryRun = true
 	classifyReq.UseCache = req.UseCache
 
-	ctx := r.Context()
-	cfg2 := s.cfg.Load()
-	if cfg2.Server.Timeout != "" {
-		if d, err := time.ParseDuration(cfg2.Server.Timeout); err == nil && d > 0 {
-			var cancel context.CancelFunc
-			ctx, cancel = context.WithTimeout(ctx, d)
-			defer cancel()
-		}
-	}
-
-	resp := s.clf.Classify(ctx, classifyReq)
+	resp := s.clf.Classify(r.Context(), classifyReq)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
